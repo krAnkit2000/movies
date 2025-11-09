2b95908709813eab5cbad459b6228295c4b63df0
ankit <ankitkumar897955@gmail.com>
2025-11-09 12:01:03 +0530

    add 2024 and 2025 movies data

---
+++ src/component/data/2025.js
@@ -0,0 +1,333 @@
+const Movies2025 = [
+  {
+    "title": "Aankhon ki Gustaakhiyan",
+    "year": 2025,
+    "genre": "Drama, Romance",
+    "poster": "https://filmfare.wwmindia.com/content/2025/jul/aankhonkigustaakhiyanre1752043154.jpg",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Aankhon%20ki%20Gustaakhiyan.mp4"
+  },
+  {
+    "title": "Aap Jaisa Koi",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Aap%20Jaisa%20Koi.mp4"
+  },
+  {
+    "title": "Agnyathavasi",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Agnyathavasi.mp4"
+  },
+  {
+    "title": "Akaal The Unconquered",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Akaal%20The%20Unconquered.mp4"
+  },
+  {
+    "title": "Akshardham Operation Vajra Shakti",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Akshardham%20Operation%20Vajra%20Shakti.mp4"
+  },
+  {
+    "title": "Azaad",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Azaad.mp4"
+  },
+  {
+    "title": "Baaghi 4",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Baaghi%204.mp4"
+  },
+  {
+    "title": "Bhagwat Chapter One Raakshas",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Bhagwat%20Chapter%20One%20Raakshas.mp4"
+  },
+  {
+    "title": "Bhool Chuk Maaf",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Bhool%20Chuk%20Maaf.mp4"
+  },
+  {
+    "title": "Bobby Aur Rishi Ki Love Story",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Bobby%20Aur%20Rishi%20Ki%20Love%20Story.mp4"
+  },
+  {
+    "title": "Chhaava",
+    "year": 2025,
+    "genre": "History, Action, ,Drama",
+    "poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0IfyAU2VKhi9idE-H29QfleuY1PmgTbmoKsk9ixU-GtYxSSLL8IUKA2wZ6x6ubTEwg7iQ&s=10",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Chhaava.mp4"
+  },
+  {
+    "title": "Detective Sherdil",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Detective%20Sherdil.mp4"
+  },
+  {
+    "title": "Deva",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Deva.mp4"
+  },
+  {
+    "title": "Devmanus",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Devmanus.mp4"
+  },
+  {
+    "title": "Dhadak 2",
+    "year": 2025,
+    "genre": "Action,Drama , Romance ,mystery & thriller",
+    "poster": "https://assets-in.bmscdn.com/iedb/movies/images/mobile/thumbnail/xlarge/dhadak-2-et00399488-1754200950.jpg",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Dhadak%202.mp4"
+  },
+  {
+    "title": "Dhoom Dhaam",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Dhoom%20Dhaam.mp4"
+  },
+  {
+    "title": "Dil Dosti Aur Dogs",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Dil%20Dosti%20Aur%20Dogs.mp4"
+  },
+  {
+    "title": "Ek Deewane Ki Deewaniyat",
+    "year": 2025,
+    "genre": "Action,Romance",
+    "poster": "https://stat4.bollywoodhungama.in/wp-content/uploads/2025/02/Ek-Deewane-Ki-Deewaniyat-poster.jpg",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Ek%20Deewane%20Ki%20Deewaniyat.mp4"
+  },
+  {
+    "title": "Eleven",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Eleven.mp4"
+  },
+  {
+    "title": "Emergency",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Emergency.mp4"
+  },
+  {
+    "title": "Fateh",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Fateh.mp4"
+  },
+  {
+    "title": "Game Changer",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Game%20Changer.mp4"
+  },
+  {
+    "title": "Greater Kalesh",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Greater%20Kalesh.mp4"
+  },
+  {
+    "title": "Ground Zero",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Ground%20Zero.mp4"
+  },
+  {
+    "title": "Hisaab Barabar",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Hisaab%20Barabar.mp4"
+  },
+  {
+    "title": "Housefull 5",
+    "year": 2025,
+    "genre": "Comedy,Romance,Drama",
+    "poster": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT4ZTzEbwKo61VYjupqjo_b7qNqa490S75kRGVawAE2KnREUuqcj8NRJIkRZEmaCwo3VBDefBLKdSjjQ9OcWOBST1wAbJl4Rvs6sulLwxA&s=10",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Housefull%205.mp4"
+  },
+  {
+    "title": "Jaat",
+    "year": 2025,
+    "genre": "Action ,Thriller",
+    "poster": "https://stat5.bollywoodhungama.in/wp-content/uploads/2024/06/Jaat.jpg",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Jaat.mp4"
+  },
+  {
+    "title": "Jewel Thief The Heist Begins",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Jewel%20Thief%20The%20Heist%20Begins.mp4"
+  },
+  {
+    "title": "Kaalidhar Laapata",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Kaalidhar%20Laapata.mp4"
+  },
+  {
+    "title": "Kaushaljis vs Kaushal",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Kaushaljis%20vs%20Kaushal.mp4"
+  },
+  {
+    "title": "Kesari Chapter 2",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Kesari%20Chapter%202.mp4"
+  },
+  {
+    "title": "Kesari Veer",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Kesari%20Veer.mp4"
+  },
+  {
+    "title": "Kuladalli Keelyavudo",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Kuladalli%20Keelyavudo.mp4"
+  },
+  {
+    "title": "Pune Highway (2024)/",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Pune%20Highway%20(2024)%2F.mp4"
+  },
+  {
+    "title": "Raid 2",
+    "year": 2025,
+    "genre": "Action,Crime,Thriller",
+    "poster": "https://mir-s3-cdn-cf.behance.net/projects/404/0bd564225141149.Y3JvcCw1NDAwLDQyMjMsMCw1MjE.jpg",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Raid%202.mp4"
+  },
+  {
+    "title": "Saiyaara",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Saiyaara.mp4"
+  },
+  {
+    "title": "Sarzameen",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Sarzameen.mp4"
+  },
+  {
+    "title": "Sikandar",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Sikandar.mp4"
+  },
+  {
+    "title": "Sitaare Zameen Par",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Sitaare%20Zameen%20Par.mp4"
+  },
+  {
+    "title": "Sky Force",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Sky%20Force.mp4"
+  },
+  {
+    "title": "Son of Sardaar 2",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Son%20of%20Sardaar%202.mp4"
+  },
+  {
+    "title": "Songs Of Paradise",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Songs%20Of%20Paradise.mp4"
+  },
+  {
+    "title": "Subham",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Subham.mp4"
+  },
+  {
+    "title": "Thamma",
+    "year": 2025,
+    "genre": "Action ,Horror,comedy , thriller, romance ",
+    "poster": "https://akm-img-a-in.tosshub.com/indiatoday/images/story/202510/thamma-advance-booking-ayushmann-rashmika-film-starts-slow--expected-to-pick-up-202650396-1x1.jpg?VersionId=es7.mZO28wvIcx4DUOs6Upq.ybBkGzKp",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Thamma.mp4"
+  },
+  {
+    "title": "Thanal",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Thanal.mp4"
+  },
+  {
+    "title": "Tumko Meri Kasam",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/Tumko%20Meri%20Kasam.mp4"
+  },
+  {
+    "title": "War 2",
+    "year": 2025,
+    "genre": "Action",
+    "poster": "https://images.unsplash.com/photo-1735643530737-9d31e09a8436?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTR8fE1PVklFUyUyMDIwMjV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=800",
+    "video": "http://103.145.232.246/Data/movies/Hindi%20dubbed/War%202.mp4"
+  }
+];
+
+export default Movies2025;
--- src/component/data/yearsmovies.js
+++ src/component/data/yearsmovies.js
@@ -4,11 +4,14 @@ import movies2021 from "./2021";
 import movies2022 from "./2022";
 import movies2023 from "./2023";
 import movies2024 from "./2024";
+import movies2025 from "./2025";
+
 const allMovies = [
   ...movies2021,
   ...movies2022,
   ...movies2023,
   ...movies2024,
+   ...movies2025,
   
 ];
 
